4d3c6c63f70316bf3beaaf658a7828bc643eb2c6
Nabin Kandel <nabinkandel@student.pu.edu.np>
2023-10-02 13:48:45 +0545

    todo-skeleton

--- 10-Array-Loops/pratices/Todo.js
+++ 10-Array-Loops/pratices/Todo.js
@@ -1,23 +1,45 @@
-const todoList = [];
+
+ const TodoListOne = [];
 
 
+function addInputOne(){
+    const inputDataOne = document.querySelector('.js-inputOne');
+    const inputData = inputDataOne.value;
+    console.log(inputData);
+    TodoListOne.push(inputData);
+    console.log(TodoListOne);
+    inputDataOne.value = '';
+};
 
 
-function addTask()  
-        {
-            let inputValue = document.querySelector('.js-inputTask') ;
-            todoList.push(inputValue.value);
-            
-            console.log(todoList);
-
-            inputValue.value = '';
-            // document.querySelector('.js-task').innerHTML = ` ${todoList} `;
-            
-        };
+// TodoListTwo 
 
 
+const TodoListTwo = [];
+
+function addInputTwo(){
+    const inputDataTwo = document.querySelector('.js-inputTwo');
+    const inputData2 = inputDataTwo.value;
+    TodoListTwo.push(inputData2);
+    inputDataTwo.value = '';
+    renderTodo();
+};
 
 
+function renderTodo(){
+    let totalList = '';
+    for(i=0; i<TodoListTwo.length; i++){
+        const todo = TodoListTwo[i];
+        const html = `<p>${todo}<button onclick="
 
+        TodoListTwo.splice(${i},1);
+        renderTodo();
 
+        ">Delete</button></p>`;
+        totalList = totalList + html;
+    }
+    document.querySelector('.js-todoList').innerHTML = totalList;
+    console.log(totalList);
+    console.log(TodoListTwo);};
 
+    
